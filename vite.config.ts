de1f66038fd46401e55b285606c635ce74e43ab9
Ammar Ahmed Butt <ammar93butt@gmail.com>
2025-05-06 04:07:34 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,25 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor_react';
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor_lucide';
+            }
+            if (id.includes('lovable-tagger')) {
+              return 'vendor_lovable_tagger';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 }));
