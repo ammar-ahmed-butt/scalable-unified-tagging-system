ebe2e8fd4759b861ebd45d16ce655c0d8bde03f1
Ammar Ahmed Butt <ammar93butt@gmail.com>
2025-05-06 04:29:15 +0500

    up

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/scalable-unified-tagging-system/",
   server: {
     host: "::",
     port: 8080,
